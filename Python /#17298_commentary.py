1fc9ba8653e5d348ac939b4449321094d1fe13db
Ji soo Jeong <jpy1030@naver.com>
2023-07-22 22:48:10 +0900

    230722

---
+++ Python /#17298_commentary.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+ans = [-1] * n
+A = list(map(int, input().split()))
+myStack = []
+
+for i in range(n):
+    while myStack and A[myStack[-1]] < A[i]:    # 오큰수 조건
+        ans[myStack.pop()] = A[i]   # 정답 리스트에 오큰수 저장
+    myStack.append(i)
+
+print(*ans)
