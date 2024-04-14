fc1397ff82e46159eb3869b003ea818cbd7d1dff
StopSoo <jpy1030@naver.com>
2024-04-15 00:47:43 +0900

    <solved> 공배수

---
+++ Programmers/Lv.0/공배수.cc
@@ -0,0 +1,13 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181936
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int number, int n, int m) {
+  int answer = 0;
+  if (number%n == 0 && number%m == 0) return 1;
+  else return 0;
+  return answer;
+}
