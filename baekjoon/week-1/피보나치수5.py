2f2abf36d04a434126f8ed9d908a3ea1fd3cde46
StopSoo <jpy1030@naver.com>
2024-12-03 00:35:26 +0900

    [BOJ] 10870_피보나치수5

---
+++ baekjoon/week-1/피보나치수5.py
@@ -0,0 +1,26 @@
+# 재귀 함수를 사용하는 일반적인 방법
+# but, 연산 횟수가 너무 많음
+def Fibonacci(n):
+  if n == 0: return 0
+  if n == 1: return 1
+  return Fibonacci(n-2) + Fibonacci(n-1)
+
+n = int(input())
+print(Fibonacci(n))
+
+# 메모이제이션을 사용하는 방법 (*)
+def Fibonacci(n):
+  global arr
+
+  if arr[n] != -1:
+    return arr[n]
+  
+  arr[n] = Fibonacci(n-2) + Fibonacci(n-1)
+  return arr[n]
+
+n = int(input())
+
+arr = [-1] * (n + 2)  # 배열 초기화
+arr[0] = 0
+arr[1] = 1
+print(Fibonacci(n))
