851b0deab08d9b8287793d8e7733597842df3a1d
StopSoo <jpy1030@naver.com>
2024-12-04 18:33:24 +0900

    [BOJ] 4779_칸토어집합

---
+++ baekjoon/week-1/칸토어집합.py
@@ -0,0 +1,31 @@
+# 재귀함수 사용
+# 반복적인 구조 파악 (*)
+# 입출력 조건을 잘 읽자 ^.^
+def f(n):
+  if n == 0:
+    return "-"
+  else:
+    return f(n-1) + " " * (3 ** (n-1)) + f(n-1)
+
+while True:
+  try:
+    n = int(input())
+    result = f(n)
+    print(result)
+  except:
+    break
+
+# bottom-up 방식 (*)
+# 시간 복잡도 계산 후 시간 초과가 아니기 때문에 이 방식도 ok
+ans = ['' for _ in range(13)] # 초기화
+ans[0] = '-'
+
+for i in range(1, 13):
+  ans[i] = ans[i-1] + (' ' * (3 ** (i-1))) + ans[i-1]
+
+while True:
+  try:
+    n = int(input())
+    print(ans[n])
+  except:
+    break
