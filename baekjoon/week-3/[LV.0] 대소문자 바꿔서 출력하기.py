25db24b0036a0d1bcddb2c521635cd268b0860e5
StopSoo <jpy1030@naver.com>
2025-01-15 18:12:44 +0900

    [PRG] 대소문자 바꿔서 출력하기

---
+++ baekjoon/week-3/[LV.0] 대소문자 바꿔서 출력하기.py
@@ -0,0 +1,31 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181949
+
+# 내 답안
+# 아스키 코드 이용 -> chr() / ord()
+str = input()
+new_str = []
+
+for c in str:
+    if c >= chr(97):
+        new_str.append(chr(ord(c) - 32))
+    else:
+        new_str.append(chr(ord(c) + 32))
+print(''.join(new_str))
+
+# 한 줄 풀이 1
+print(input().swapcase())
+
+# 한 줄 풀이 2
+print(''.join(x.upper() if x == x.lower() else x.lower() for x in input()))
+
+# upper() / lower() 라이브러리 사용
+str = input()
+a = ''
+
+for s in str:
+    if (s.isupper()):
+        a = a + s.lower()
+    else: 
+        a = a + s.upper()
+
+print(a)
