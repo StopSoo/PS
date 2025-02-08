69c06e36ba79dd53888d44f3927ced92112652e8
StopSoo <jpy1030@naver.com>
2025-02-09 00:18:07 +0900

    [BOJ] <solved> 2869_달팽이는올라가고싶다

--- 2025-PS/week-6/[BRONZE I] 달팽이는올라가고싶다.py
+++ 2025-PS/week-6/[BRONZE I] 달팽이는올라가고싶다.py
@@ -1,13 +1,13 @@
 # https://www.acmicpc.net/problem/2869
 
 # 내 답안
+# 반복문을 사용하지 않고, 수학적으로 접근 (!)
 A, B, V = map(int, input().split())
-answer = 1 # 날짜 수
-height = 0
-while height != V:
-  height += A
-  if height >= V: break
-  height -= B
-  if height >= V: break
-  answer += 1
-print(answer)
+# (A - B) * i + A >= V
+print((V - A) // (A - B) + 1 if (V - A) % (A - B) == 0 else (V - A) // (A - B) + 2)
+
+# 다른 사람들의 답안
+# 올림 함수 사용하기 (!)
+import math
+A, B, V = map(int, input().split())
+print(math.ceil((V - B) / (A - B)))
