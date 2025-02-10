a49550d219fd1b557b7cd057eb80f45aede3bd26
StopSoo <jpy1030@naver.com>
2025-02-10 20:08:01 +0900

    [BOJ] <solved> 2485_가로수

--- 2025-PS/week-6/[SILVER IV] 가로수.py
+++ 2025-PS/week-6/[SILVER IV] 가로수.py
@@ -6,5 +6,27 @@ N = int(input())
 positions = [int(input()) for _ in range(N)]
 
 gaps = []
+answer = 0
 for i in range(len(positions)-1): gaps.append(positions[i+1] - positions[i])
-gap = gcd(gaps[0], gaps[1])
+gap = gcd(gaps[0], gaps[1]) # 초기화
+for i in range(2, len(gaps)): gap = gcd(gap, gaps[i])
+for i in range(len(positions)-1): answer += (positions[i+1] - positions[i]) // gap - 1
+print(answer)
+
+# 다른 사람의 답안
+# gcd에서 인수로 배열을 넣을 때 *를 사용 (!)
+import math
+
+def solution(tree):
+    n = len(tree)
+    distances = [tree[i] - tree[i - 1] for i in range(1, n)]
+    gcd_value = math.gcd(*distances)
+
+    result = 0
+    for i in distances:
+        result += (i // gcd_value) - 1
+    return result
+
+N = int(input())
+trees = [int(input()) for _ in range(N)]
+print(solution(trees))
