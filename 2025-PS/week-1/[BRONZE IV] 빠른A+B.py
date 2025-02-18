a0fca58b0392396543cc7bb3b1309b4af5ae3693
StopSoo <jpy1030@naver.com>
2025-02-18 17:28:02 +0900

    [BOJ] <solved> 15552_빠른 A+B

--- 2025-PS/week-1/[BRONZE IV] 빠른A+B.py
+++ 2025-PS/week-1/[BRONZE IV] 빠른A+B.py
@@ -1,8 +1,12 @@
-import sys
+# https://www.acmicpc.net/problem/15552
+
+# 내 답안
 # 빠른 입출력을 위해 sys.stdin.readline()을 사용
 # rstrip(): 맨 끝 개행문자를 제거
-T = int(sys.stdin.readline().rstrip())
+import sys
+input = sys.stdin.readline
+T = int(input().rstrip())
 
 for i in range(T):
-  a, b = map(int, sys.stdin.readline().rstrip().split())
+  a, b = map(int, input().rstrip().split())
   print(a + b)
