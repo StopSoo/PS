19bab8ce306b8cdb15d1b7200bf56e2c3bf41120
StopSoo <jpy1030@naver.com>
2025-02-24 14:17:08 +0900

    [BOJ] <solved> 10798_세로 읽기

--- 2025-PS/week-9/[BRONZE I] 세로읽기.py
+++ 2025-PS/week-9/[BRONZE I] 세로읽기.py
@@ -1,6 +1,8 @@
 # https://www.acmicpc.net/problem/10798
 
 # 내 답안
+# 걸린 시간: 40ms
+# 파이썬 리스트에서 인덱스로 제거: del 요소
 words = [list(input()) for _ in range(5)]
 
 answer = ""
@@ -8,7 +10,7 @@ while True:
   for word in words:
     if word != []:
       answer += word[0]
-      words[0] = words[0][1:]
+      del word[0]
   if words == [[], [], [], [], []]:
     break
 print(answer)
