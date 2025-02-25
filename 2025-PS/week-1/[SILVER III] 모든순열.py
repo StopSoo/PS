e1a17f2bbfc25b6a67827259437c9e36aecf3e18
StopSoo <jpy1030@naver.com>
2025-02-25 17:31:22 +0900

    [BOJ] <solved> 10974_모든 순열

--- 2025-PS/week-1/[SILVER III] 모든순열.py
+++ 2025-PS/week-1/[SILVER III] 모든순열.py
@@ -1,4 +1,7 @@
-# permutations(arr, r)에서 r은 사용하지 않는다면 생략 가능
+# https://www.acmicpc.net/problem/10974
+
+# 내 답안
+# permutations(arr, r)에서 r은 사용하지 않는다면 생략 가능 -> 기본값으로 배열의 길이가 들어감
 # 출력문 형태에 유의할 것 (*)
 from itertools import permutations
 
