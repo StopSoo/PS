80db0d030d60fe3b717b5583e93c88c7b386aadf
StopSoo <jpy1030@naver.com>
2025-02-28 17:18:49 +0900

    [BOJ] <unsolved> 9375_패션왕 신해빈

---
+++ 2025-PS/week-9/[SILVER III] 패션왕신해빈.py
@@ -0,0 +1,33 @@
+# https://www.acmicpc.net/problem/9375
+
+# 내 답안
+# 걸린 시간: 32ms
+# 처음 코드 -> combinations를 사용해서 모든 조합을 다 계산 -> 불필요한 연산. 고로 시간 초과가 남.
+# 아이디어: 각 종류에서 경우의 수는 (해당 종류의 의상 개수 + 1)이다 (!) -> 선택 안함이 포함됨.
+from itertools import combinations
+import sys
+input = sys.stdin.readline
+
+T = int(input().strip())
+for _ in range(T):
+  dt = dict()
+  n = int(input().strip())
+  
+  for _ in range(n):
+    name, case = input().strip().split()
+    dt[case] = dt.get(case, 0) + 1 # 아래보다 좀 더 개선된 코드
+    # if case in dt.keys():
+    #   dt[case] += 1
+    # else:
+    #   dt[case] = 1
+  answer = 1
+  for count in dt.values():
+    answer *= (count + 1)
+  print(answer - 1)
+  # for i in range(1, len(dt.keys())+1): # 종류 고르는 개수
+  #   for comb in combinations(dt.keys(), i): # 종류 고르는 조합
+  #     mul = 1
+  #     for case in comb:
+  #       mul *= dt[case]
+  #     answer += mul
+  # print(answer)
