71ad2b7726ef9925cc422d1d4bbc3d23098aed04
StopSoo <jpy1030@naver.com>
2025-03-12 15:36:32 +0900

    [BOJ] <solved> 11054_가장 긴 바이토닉 부분 수열

--- 2025-PS/week-11/[GOLD IV] 가장긴바이토닉부분수열.py
+++ 2025-PS/week-11/[GOLD IV] 가장긴바이토닉부분수열.py
@@ -5,17 +5,17 @@
 # 11053_가장긴증가하는부분수열 문제를 참고하면 됨 (!) 하지만 제대로 이해하진 못함 ...
 N = int(input())
 numbers = list(map(int, input().split()))
-dp1 = [0] * N
-dp2 = [0] * N
+dp1 = [0] * N # 증가하는 가장 긴 부분 수열
+dp2 = [0] * N # 감소하는 가장 긴 부분 수열
 dp1[0] = dp2[N-1] = 1
 # dp1
-for n in range(1, N):
+for n in range(1, N): # 1 ~ N-1
   dp1[n] = 1
   for i in range(0, n): 
     if numbers[i] < numbers[n]:
       dp1[n] = max(dp1[n], dp1[i]+1)
 # dp2
-for n in range(N-2, -1, -1):
+for n in range(N-2, -1, -1): # N-2 ~ 0
   dp2[n] = 1
   for i in range(N-1, n, -1):
     if numbers[i] < numbers[n]:
