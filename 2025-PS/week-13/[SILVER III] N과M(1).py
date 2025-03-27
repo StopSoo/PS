606aa774215f30ec1309f1bc5c168120bddf8331
StopSoo <jpy1030@naver.com>
2025-03-27 17:24:04 +0900

    [BOJ] <solved> 15649_N과 M(1)

---
+++ 2025-PS/week-13/[SILVER III] N과M(1).py
@@ -0,0 +1,26 @@
+# https://www.acmicpc.net/problem/15649
+
+# 내 답안
+# 걸린 시간: 132ms
+from itertools import permutations
+
+N, M = map(int, input().split())
+for comb in permutations(range(1, N+1), M):
+  print(*comb)
+
+# 백트래킹으로 구현한 코드
+# 걸린 시간: 144ms
+N, M = map(int, input().split())
+ans = []
+
+def back():
+  if len(ans) == M: # 배열의 길이를 확인
+    print(" ".join(map(str, ans))) # 1 2 3 이런 상태로 출력하기 위해
+    return 
+  for i in range(1, N+1): # 1 ~ N 까지
+    if i not in ans: # 중복 확인
+      ans.append(i) # 배열 추가
+      back() # 재귀
+      ans.pop() # return으로 돌아오면 이게 실행됨. 1, 2, 3 일때 3을 없앰으로 전 단계로 돌아가는 것
+
+back()
