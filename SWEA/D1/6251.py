63e3cc49275074a668310cbf8421f4eb107b1e84
StopSoo <jpy1030@naver.com>
2025-05-13 16:14:31 +0900

    [SWEA] <solved> 6251_[파이썬 프로그래밍 기초(1) 파이썬의 기본 구조와 기초 문법] 7. 흐름과 제어 - 반복 11

---
+++ SWEA/D1/6251.py
@@ -0,0 +1,6 @@
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=1&contestProbId=AWcVEC-K4uADFAU4&categoryId=AWcVEC-K4uADFAU4&categoryType=CODE&problemTitle=&orderBy=FIRST_REG_DATETIME&selectCodeLang=ALL&select-1=1&pageSize=10&pageIndex=1
+
+# 내 답안
+# 걸린 시간:
+for i in range(1, 6):
+    print(' ' * (5-i) + '*' * i)
