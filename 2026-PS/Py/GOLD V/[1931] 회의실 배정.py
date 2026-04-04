ccc03253e4455401ed3b880e19ce888ed158cb33
StopSoo <jpy1030@naver.com>
2026-04-04 16:37:42 +0900

    [BOJ] <reviewed> 1931_회의실 배정

--- 2026-PS/Py/GOLD V/[1931] 회의실 배정.py
+++ 2026-PS/Py/GOLD V/[1931] 회의실 배정.py
@@ -19,3 +19,20 @@ for s, e in times[1:]:
         count += 1
 
 print(count)
+
+# 260404 풀이 (복습)
+# 탐욕적 선택 속성을 활용한 "종료 시간이 이른 것, 종료 시간이 같다면 시작 시간이 이른 것" 기준으로 정렬!
+import sys
+input = sys.stdin.readline
+
+N = int(input()) # 회의 수
+times = sorted([list(map(int, input().strip().split())) for _ in range(N)], key=lambda x: [x[1], x[0]])
+
+count = 1 # 첫 번째로 끝나는 회의 선택
+end_time = times[0][1] 
+for s, e in times[1:]:
+    if s >= end_time:
+        count += 1
+        end_time = e
+
+print(count)
